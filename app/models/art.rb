973b8d8e69388e7844b566188bf117deefa669f1
Alexandre Magalhaes <alexandremagalhaes99@gmail.com>
2021-11-22 16:58:00 -0300

    added the backend configuration for cloudinary

--- app/models/art.rb
+++ app/models/art.rb
@@ -2,6 +2,7 @@ class Art < ApplicationRecord
   has_many :transactions
   has_many :bids
   belongs_to :user
+  has_one_attached :photo
 
   validates :title, presence: true
   validates :description, presence: true
