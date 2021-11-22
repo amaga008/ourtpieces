3b8287eed15133dc80703421e74622825e3045af
Alexandre Magalhaes <alexandremagalhaes99@gmail.com>
2021-11-22 16:41:38 -0300

    added some validations and associations

--- app/models/art.rb
+++ app/models/art.rb
@@ -1,2 +1,12 @@
 class Art < ApplicationRecord
+  has_many :transactions
+  has_many :bids
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :starting_price, presence: true
+  validates :category, presence: true
+
+  # We need to see how validation will work out with the auctions part
 end
--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,6 @@
 class Bid < ApplicationRecord
   belongs_to :user
   belongs_to :art
+
+  validates :amount, presence: true
 end
