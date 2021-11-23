48aa26b3e8cc4c96df7cbfd355bc3b75765a19d4
Alexandre Magalhaes <alexandremagalhaes99@gmail.com>
2021-11-23 11:41:35 -0300

    added the first routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show]
+
+  resources :arts do
+    resources :transactions, only: [ :create ]
+    resources :bids, only: [ :create ]
+  end
+  resources :bids, only: [:show, :destroy]
 end
