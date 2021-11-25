d4d9c97da2e627290566acd0d7a6a9379310861a
Ada Puric <puric.ada1@gmail.com>
2021-11-25 16:15:55 +0000

    show page edits

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,9 +28,12 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
+import {modals} from "../modals/modals"
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  modals()
 });
 
 import "controllers"
