1afd3c2d51be02a738e46c12b3a8a8982af685f1
Franziska Lippitt <lippittfranziska@gmail.com>
2021-11-25 15:11:59 +0000

    first part done of the create page

---
+++ app/javascript/forms/checkbox.js
@@ -0,0 +1,9 @@
+const isAuction = () => {
+  const checkboxSelector = document.querySelector("#art_is_for_auction")
+  const auctionSelector = document.querySelector(".art_auction_end_time")
+  checkboxSelector.addEventListener("click", () => {
+    auctionSelector.classList.toggle("visible")
+  })
+}
+
+export {isAuction}
