d72433ce5ec05cfcf7b8771de526f9a9022e8827
Maximiliaan Michielsen <max.michielsen@gmail.com>
2021-11-26 11:39:08 +0000

    stash

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,13 @@ end
 
 10.times do
   art = Art.create!(
-     title: Faker::Space.constellation,
-     description: Faker::Lorem.paragraph,
-     category: ["paintings", "sculptures", "pottery", "photography", "ceramics"].sample,
-     user_id: User.all.sample.id,
-     measurements: ["100x400", "600x750", "1000x1450", "2000x4350"].sample,
-     starting_price: [500, 600, 700, 1000].sample,
-     creator: Faker::Artist.name
-   )
-   puts "Creating arts with id #{art.id}"
- end
+    title: Faker::Space.constellation,
+    description: Faker::Lorem.paragraph,
+    category: ["paintings", "sculptures", "pottery", "photography", "ceramics"].sample,
+    user_id: User.all.sample.id,
+    measurements: ["100x400", "600x750", "1000x1450", "2000x4350"].sample,
+    starting_price: [500, 600, 700, 1000].sample,
+    creator: Faker::Artist.name
+  )
+  puts "Creating arts with id #{art.id}"
+end
