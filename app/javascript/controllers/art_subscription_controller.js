ef1d9c0743ae0fab2ea98d3d2b46efdfbecc50f6
Alexandre Magalhaes <alexandremagalhaes99@gmail.com>
2021-12-01 15:43:44 -0300

    fixing the table

--- app/javascript/controllers/art_subscription_controller.js
+++ app/javascript/controllers/art_subscription_controller.js
@@ -1,14 +1,18 @@
 import { Controller } from "stimulus";
 import consumer from "../channels/consumer";
-
+const auction = document.querySelector("table")
+console.log(auction.childNodes[1].childNodes[3])
 export default class extends Controller {
   static values = { artId: Number }
 
   connect() {
     this.channel = consumer.subscriptions.create(
       { channel: "ArtChannel", id: this.artIdValue },
-      { received: data => this.element.insertAdjacentHTML("beforeend", data)}
+      { received: data => auction.insertAdjacentHTML("beforeend", data) }
     )
   console.log(`Subscribed to the art with the id ${this.artIdValue}.`);
   }
 }
+
+// console.log("x", data, this.element)
+// this.element.children[0].children[0].children[1].insertAdjacentHTML("afterbegin", data)}
