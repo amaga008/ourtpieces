3beee8c5192fe26f37a6acd7a7d32b444ab7f27a
Alexandre Magalhaes <alexandremagalhaes99@gmail.com>
2021-12-02 12:11:27 -0300

    fixed js bid

--- app/javascript/components/bid.js
+++ app/javascript/components/bid.js
@@ -7,7 +7,7 @@ const winningBid = () => {
       if (winningBid) {
         const lostBid = winningBid.nextElementSibling
         winningBid.classList.add("winning-bid")
-        if (lostBid.classList.contains("winning-bid")) {
+        if (lostBid && lostBid.classList.contains("winning-bid")) {
          lostBid.classList.remove("winning-bid")
         }
       }
