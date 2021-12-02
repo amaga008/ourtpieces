31d62970d9e77639a315953d8d089a5864dabada
Ada Puric <puric.ada1@gmail.com>
2021-12-02 10:20:40 +0000

    fixed auction table

--- app/javascript/controllers/art_subscription_controller.js
+++ app/javascript/controllers/art_subscription_controller.js
@@ -1,14 +1,14 @@
 import { Controller } from "stimulus";
 import consumer from "../channels/consumer";
-const auction = document.querySelector("table")
-console.log(auction.childNodes[1].childNodes[3])
+const auctionTable = document.querySelector(".bids")
+
 export default class extends Controller {
   static values = { artId: Number }
 
   connect() {
     this.channel = consumer.subscriptions.create(
       { channel: "ArtChannel", id: this.artIdValue },
-      { received: data => auction.insertAdjacentHTML("beforeend", data) }
+      { received: data => auctionTable.insertAdjacentHTML("afterbegin", data) }
     )
   console.log(`Subscribed to the art with the id ${this.artIdValue}.`);
   }
